9003eed71b15f18928e00095e7a3aefc7bfeded0
braojntu <braojntu@gmail.com>
2021-10-23 01:40:11 +0530

    ver. 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
 
-function App() {
+const pricecard = [
+  {
+    plan: "FREE",
+    price: "$0",
+    period: "/month",
+    features: {
+      users: ["Single User", "Multiple User"],
+      storage: ["5GB Storgae"],
+      pubprojects: ["Unlimited Public Projects"],
+      accesstype: ["Cummunity Access"],
+      privprojects: ["Unlimited Private Projects"],
+      support: ["Dedicated Phone Support"],
+      domain: ["Free Subdomain"],
+      reports: ["Monthly Status Reports"]
+    }
+  },
+  {
+    plan: "PLUS",
+    price: "$9",
+    period: "/month",
+    features: {
+      users: ["5 Users"],
+      storage: ["50GB Storgae"],
+      pubprojects: ["Unlimited Public Projects"],
+      accesstype: ["Cummunity Access"],
+      privprojects: ["Unlimited Private Projects"],
+      support: ["Dedicated Phone Support"],
+      domain: ["Free Subdomain"],
+      reports: ["Monthly Status Reports"]
+    }
+  },
+  {
+    plan: "PRO",
+    price: "$49",
+    period: "/month",
+    features: {
+      users: ["Unlimited Users"],
+      storage: ["150GB Storgae"],
+      pubprojects: ["Unlimited Public Projects"],
+      accesstype: ["Cummunity Access"],
+      privprojects: ["Unlimited Private Projects"],
+      support: ["Dedicated Phone Support"],
+      domain: ["Unlimited Free Subdomain"],
+      reports: ["Monthly Status Reports"]
+    }
+  }
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Subscription Plans</h1>
+      <Plans />
     </div>
   );
 }
 
-export default App;
+function Plans() {
+  return (
+    <div className="card-cont">
+      {pricecard.map((plan) => (
+        <Card
+          plan={plan.plan}
+          price={plan.price}
+          period={plan.period}
+          features={plan.features}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div>
+        <p className="plan">{props.plan}</p>
+      </div>
+
+      <div>
+        <p className="price">
+          <span className="amt">{props.price} </span>
+          <span className="period">{props.period} </span>
+        </p>
+      </div>
+      <div className="features">
+        <ul>
+          {Object.keys(props.features).map((key) => (
+            <li className="f-list">{props.features[key].join(", ")}</li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="btn-cont">
+        <button className="button">BUY</button>
+      </div>
+    </div>
+  );
+}
